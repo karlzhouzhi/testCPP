16c33cc0b64b6f9f8eeae2b4685fc34f6ab9ed74
zhouzhi96 <zhouzhi96@jd.com>
2023-11-29 15:01:14 +0800

    1

---
+++ third/build/CMakeFiles/third.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "/Users/zhouzhi96/work/source/business/cpp/testCPP/third/lib/.1"
+  "/Users/zhouzhi96/work/source/business/cpp/testCPP/third/lib/libthird.1.0.0.dylib"
+  "/Users/zhouzhi96/work/source/business/cpp/testCPP/third/lib/libthird.1.dylib"
+  "/Users/zhouzhi96/work/source/business/cpp/testCPP/third/lib/libthird.dylib"
+  "/Users/zhouzhi96/work/source/business/cpp/testCPP/third/lib/libthird.pdb"
+  "CMakeFiles/third.dir/src/add.cpp.o"
+  "CMakeFiles/third.dir/src/add.cpp.o.d"
+  "CMakeFiles/third.dir/src/multiply.cpp.o"
+  "CMakeFiles/third.dir/src/multiply.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/third.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
